b8e91aabfcd3347f17e044f64ba90522d4733826
Christoph Deil <Deil.Christoph@gmail.com>
2017-06-07 15:21:15 +0200

    Remove dummy test function

--- hips/draw/tests/test_simple.py
+++ hips/draw/tests/test_simple.py
@@ -1,7 +1,1 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
-from ..simple import hello
-
-
-def test_hello():
-    meaning_of_life = hello('Adeel')
-    assert meaning_of_life == 43
