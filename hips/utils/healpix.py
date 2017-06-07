0b1359036a1436551234dd21ce47149c533c1bea
Adeel Ahmad <adeelahmad14@hotmail.com>
2017-06-07 14:12:46 +0500

    Update utils.healpix module
    This commit addresses import conventions and also some formatting issues. An example was also added and the test case was updated to follow this issue: https://github.com/healpy/healpy/issues/393#issuecomment-305994042.

--- hips/utils/healpix.py
+++ hips/utils/healpix.py
@@ -9,14 +9,14 @@ __all__ = [
     'boundaries',
 ]
 
-import healpy
+import healpy as hp
 import numpy as np
 
 
 def boundaries(nside: int, pix: int, nest: bool=False) -> tuple:
     """Returns an array containing the angle (theta and phi) in radians.
 
-    This method calls `healpy.boundaries` and `healpy.vec2ang`.
+    This function calls `healpy.boundaries` and `healpy.vec2ang`.
 
     Parameters
     ----------
@@ -33,14 +33,21 @@ def boundaries(nside: int, pix: int, nest: bool=False) -> tuple:
         Returns the angle (theta and phi) in radians
 
     Examples
-    -------
-    nside = 8
-    pix = 450
-    theta, phi = boundaries(nside, pix, nest=True)
+    --------
+    ::
 
-    radec = SkyCoord(ra=phi, dec=np.pi/2 - theta, unit='radian', frame='icrs')
+        >>> import numpy as np
+        >>> from hips.utils import boundaries
+        >>> from astropy.coordinates import SkyCoord
+        >>> nside = 8
+        >>> pix = 450
+        >>> theta, phi = boundaries(nside, pix, nest=True)
+        >>> SkyCoord(ra=phi, dec=np.pi/2 - theta, unit='radian', frame='icrs')
+        <SkyCoord (ICRS): (ra, dec) in deg
+        [( 264.375, -24.62431835), ( 258.75 , -30.        ),
+         ( 264.375, -35.68533471), ( 270.   , -30.        )]>
     """
 
-    boundary_coords = healpy.boundaries(nside, pix, nest=nest)
-    theta, phi = healpy.vec2ang(np.transpose(boundary_coords))
+    boundary_coords = hp.boundaries(nside, pix, nest=nest)
+    theta, phi = hp.vec2ang(np.transpose(boundary_coords))
     return theta, phi
--- hips/utils/tests/test_healpix.py
+++ hips/utils/tests/test_healpix.py
@@ -1,15 +1,24 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
+import numpy as np
+import healpy as hp
 from .. healpix import boundaries
 from astropy.coordinates import SkyCoord
-import numpy as np
+from numpy.testing import assert_allclose
 
 
 def test_boundaries():
-    nside = 8
+    order = 3
+    nside = hp.order2nside(order)
     pix = 450
     theta, phi = boundaries(nside, pix, nest=True)
 
     radec = SkyCoord(ra=phi, dec=np.pi/2 - theta, unit='radian', frame='icrs')
+
+    """
+    These HEALPix corner values were verified through Aladin Lite with the "Show
+    healpix grid" option turned on. More information can be found on this GitHub
+    issue: https://github.com/healpy/healpy/issues/393#issuecomment-305994042
+    """
     radec_precomp = [[264.375,  258.75,  264.375,  270.],
                      [-24.624318,  -30.,  -35.685335,  -30.]]
-    np.testing.assert_allclose([radec.ra, radec.dec], radec_precomp)
+    assert_allclose([radec.ra, radec.dec], radec_precomp)
