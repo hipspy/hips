1f0b0dfb873e83e3af0d432e49a245ec650636da
Adeel Ahmad <adeelahmad14@hotmail.com>
2017-06-14 09:27:43 +0500

    Remove blank lines and docstring

--- hips/tiles/hips.py
+++ hips/tiles/hips.py
@@ -1,7 +1,4 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
-"""Classes and functions to manage HiPS tiles."""
-
-
 from .description import HipsDescription
 import numpy as np
 
