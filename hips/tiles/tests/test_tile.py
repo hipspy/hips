b937d1486e2c67c2519c8b572b43e899536ca24c
Adeel Ahmad <adeelahmad14@hotmail.com>
2017-06-20 14:30:02 +0500

    Update tiles.hips module

--- hips/tiles/tests/test_tile.py
+++ hips/tiles/tests/test_tile.py
@@ -1,5 +1,6 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 from astropy.utils.data import get_pkg_data_filename
+from astropy.tests.helper import remote_data
 from ..description import HipsDescription
 from ..tile import HipsTile
 from pathlib import Path
@@ -11,38 +12,50 @@ class TestHipsTile:
         filename = get_pkg_data_filename('data/properties.txt')
         hips_description = HipsDescription.read(filename)
 
-        cls.tile = HipsTile(hips_description, 6, 24185, 'jpg', 512)
-        cls.tile.base_url = 'http://alasky.unistra.fr/DSS/DSSColor'
+        cls.fits_tile = HipsTile(hips_description=hips_description, order=6,
+                            ipix=30889, format='fits', tile_width=512)
+        cls.jpg_tile = HipsTile(hips_description=hips_description, order=6,
+                            ipix=30889, format='jpg', tile_width=512)
+        cls.fits_tile.base_url = 'http://alasky.unistra.fr/2MASS/H'
+        cls.jpg_tile.base_url = 'http://alasky.unistra.fr/2MASS/H'
 
     def test_base_url(self):
-        assert self.tile.base_url == 'http://alasky.unistra.fr/DSS/DSSColor'
+        assert self.fits_tile.base_url == 'http://alasky.unistra.fr/2MASS/H'
+        assert self.jpg_tile.base_url == 'http://alasky.unistra.fr/2MASS/H'
 
     def test_tile_url(self):
-        assert self.tile.tile_url == 'http://alasky.unistra.fr/DSS/DSSColor/Norder6/Dir20000/Npix24185.jpg'
+        assert self.fits_tile.tile_url == 'http://alasky.unistra.fr/2MASS/H/Norder6/Dir30000/Npix30889.fits'
+        assert self.jpg_tile.tile_url == 'http://alasky.unistra.fr/2MASS/H/Norder6/Dir30000/Npix30889.jpg'
 
+    @remote_data
     def test_fetch(self):
-        self.tile.fetch()
+        self.fits_tile.fetch()
+        self.jpg_tile.fetch()
 
-        """
-        This data was obtain from 'http://alasky.unistra.fr/DSS/DSSColor/Norder6/Dir20000/Npix24185.jpg'
-        at the index [10, 20]
-        """
-        data_precomp = [0, 0, 0]
-        assert self.data[10, 20] == data_precomp
+        shape_fits_precomp = (512, 512)
+        shape_jpg_precomp = (512, 512, 3)
+        data_precomp = [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1]
+        assert self.fits_tile.data.shape == shape_fits_precomp
+        assert self.jpg_tile.data.shape == shape_jpg_precomp
+        assert list(self.fits_tile.data[510][:12]) == data_precomp
 
     def test_read(self):
-        self.tile.read()
+        self.fits_tile.read()
+        self.jpg_tile.read()
 
-        """
-        This data was obtain from 'http://alasky.unistra.fr/DSS/DSSColor/Norder6/Dir20000/Npix24185.jpg'
-        at the index [10, 20]
-        """
-        data_precomp = [0, 0, 0]
-        assert list(self.tile.data[10, 20]) == data_precomp
+        shape_fits_precomp = (512, 512)
+        shape_jpg_precomp = (512, 512, 3)
+        data_precomp = [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1]
+        assert (self.fits_tile.data.shape) == shape_fits_precomp
+        assert (self.jpg_tile.data.shape) == shape_jpg_precomp
+        assert list(self.fits_tile.data[510][:12]) == data_precomp
 
-    def test_store(self):
+    def test_write(self):
         filename = 'test_file'
-        self.tile.store(filename)
+        self.fits_tile.write(filename)
+        self.jpg_tile.write(filename)
 
-        path = self.tile.path.joinpath(''.join([filename, '.', self.tile.format]))
-        assert True == path.is_file()
+        path_fits = self.fits_tile.path.joinpath(''.join([filename, '.', self.fits_tile.format]))
+        path_jpg = self.jpg_tile.path.joinpath(''.join([filename, '.', self.jpg_tile.format]))
+        assert True == path_fits.is_file()
+        assert True == path_jpg.is_file()
--- hips/tiles/tile.py
+++ hips/tiles/tile.py
@@ -1,12 +1,11 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
-from astropy.utils.data import get_pkg_data_filename
 from .description import HipsDescription
 from astropy.io import fits
 from pathlib import Path
 from io import BytesIO
 from PIL import Image
-import numpy as np
 import urllib.request
+import numpy as np
 
 __all__ = [
     'HipsTile',
@@ -17,8 +16,8 @@ class HipsTile:
     """HiPS tile container.
 
     This class provides methods for fetching, reading,
-    and stroing a HiPS tile. It also contains a few
-    getters and setters around most commonly used
+    and writing a HiPS tile. It also contains a few
+    getters and setters around frequently used
     HiPS tile attributes.
 
     Parameters
@@ -26,53 +25,53 @@ class HipsTile:
     hips_description : `HipsDescription`
         Class HipsDescription contains HiPS properties
     order : `int`
-        Order of the HiPS
+        Order of the HiPS tile
     ipix : `int`
         HEALPix pixel number
-    tile_width : `int`
-        Width of HiPS tile
     format : `format`
         Format of HiPS tile
-    data : `list`
+    data : `numpy.ndarray`
         Pixel values of HiPS tile
+    tile_width : `int`
+        Width of HiPS tile
+    base_url : `str`
+        Base URL of HiPS tile
 
     Examples
     --------
     ::
 
+        >>> import urllib.request
         >>> from hips.tiles import HipsDescription
-        >>> from astropy.utils.data import get_pkg_data_filename
-        >>> filename = get_pkg_data_filename('tests/data/properties.txt')
-        >>> hips_description = HipsDescription.read(filename)
-        >>> hips_tile = HipsTile(hips_description, 6, 24185, 'fits')
-        >>> tile.read()
-        >>> tile.data
-        [[3617 3304 4196 ..., 4545 4800 5701]
-         [3638 3288 3934 ..., 4448 5029 4951]
-         [3116 3653 3412 ..., 5282 4406 4967]
-         ...,
-         [3890 3463 3448 ..., 5980 6902 6543]
-         [4845 4065 3821 ..., 6928 6557 7197]
-         [4261 3581 3842 ..., 7095 6390 6191]]
-    """
+        >>> from astropy.tests.helper import remote_data
+        >>> text = urllib.request.urlopen('https://raw.githubusercontent.com/hipspy/hips/master/hips/tiles/tests/data/properties.txt').read() # doctest: +REMOTE_DATA
+        >>> hips_description = HipsDescription.parse(str(text))
+        >>> hips_tile = HipsTile(hips_description=hips_description, order=6, ipix=30889, format='fits', tile_width=512)
+        >>> hips_tile.read()
+        >>> hips_tile.data
+        array([[0, 0, 0, ..., 0, 0, 0],
+               [0, 0, 0, ..., 0, 0, 0],
+               [0, 0, 0, ..., 0, 0, 0],
+               ...,
+               [0, 0, 0, ..., 1, 0, 0],
+               [0, 0, 0, ..., 1, 0, 1],
+               [0, 0, 0, ..., 1, 0, 1]], dtype=int16)
+        """
 
-    def __init__(self, hips_description: HipsDescription, order: int, ipix: int, format: str, data: list=None, tile_width: int=512) -> None:
+    def __init__(self, hips_description: HipsDescription, order: int, ipix: int, format: str,
+                 data: np.ndarray=None, tile_width: int=512, base_url: str=None) -> None:
         self.hips_description = hips_description
         self.order = order
         self.ipix = ipix
         self.tile_width = tile_width
         self.format = format
         self.data = data
+        self.base_url = base_url
 
     @staticmethod
     def _directory(ipix: int) -> int:
         """Directory of the HiPS tile (`int`)."""
         return np.around(ipix, decimals=-(len(str(ipix)) - 1))
-
-    @property
-    def base_url(self) -> str:
-        """HiPS tile base url (`str`)."""
-        return self._base_url
 
     @property
     def path(self) -> Path:
@@ -82,17 +81,13 @@ class HipsTile:
     @property
     def tile_url(self) -> str:
         """HiPS tile url (`str`)."""
-        return ''.join([self._base_url, '/Norder', str(self.order), '/Dir', str(self._directory(self.ipix)), '/Npix', str(self.ipix), '.', self.format])
-
-    @base_url.setter
-    def base_url(self, base_url: str) -> None:
-        """Set the HiPS tile base url (`None`)."""
-        self._base_url = base_url
+        return ''.join([self.base_url, '/Norder', str(self.order), '/Dir',
+               str(self._directory(self.ipix)), '/Npix', str(self.ipix), '.', self.format])
 
     def fetch(self) -> None:
         """Fetch HiPS tile and load into memory (`None`)."""
         raw_image = BytesIO(urllib.request.urlopen(self.tile_url).read())
-        if self.format is 'fits':
+        if self.format == 'fits':
             hdulist = fits.open(raw_image)
             self.data = np.array(hdulist[0].data)
             self.header = hdulist[0].header
@@ -104,15 +99,15 @@ class HipsTile:
         path = self.path.joinpath(''.join(['Npix', str(self.ipix), '.', self.format]))
         with path.open('rb') as rf:
             raw_img = BytesIO(rf.read())
-            if self.format is 'fits':
+            if self.format == 'fits':
                 hdulist = fits.open(raw_img)
                 self.data = np.array(hdulist[0].data)
                 self.header = hdulist[0].header
             else:
                 self.data = np.array(Image.open(raw_img))
 
-    def store(self, filename: str) -> None:
-        """Store HiPS tile by a given filename (`None`).
+    def write(self, filename: str) -> None:
+        """Write HiPS tile by a given filename (`None`).
 
         Parameters
         ----------
@@ -120,12 +115,10 @@ class HipsTile:
             Name of the file
         """
         path = self.path.joinpath(''.join([filename, '.', self.format]))
-        print(path)
-        with path.open('w') as wf:
-            if self.format is 'fits':
-                hdu = fits.PrimaryHDU(self.data, header=self.header)
-                hdulist = fits.HDUList([hdu])
-                hdulist.writeto(wf)
-                hdulist.close()
-            else:
-                Image.fromarray(self.data).save(wf)
+        if self.format == 'fits':
+            hdu = fits.PrimaryHDU(self.data, header=self.header)
+            hdulist = fits.HDUList([hdu])
+            hdulist.writeto(path)
+            hdulist.close()
+        else:
+            Image.fromarray(self.data).save(str(path))
