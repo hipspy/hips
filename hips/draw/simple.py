06e676e4369a6273b68dba5c6839f3db1bca83a7
Christoph Deil <Deil.Christoph@gmail.com>
2017-06-24 11:32:49 +0200

    Continue make_sky_image and draw_sky_image implementation

--- hips/draw/simple.py
+++ hips/draw/simple.py
@@ -1,23 +1,82 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 """HiPS tile drawing -- simple method."""
-
-import healpy as hp
-
-from ..tiles import HipsSurveyProperties
-from ..tiles import HipsTile
-from ..tiles import HipsTileMeta
-from ..utils import WCSGeometry
-from ..utils import compute_healpix_pixel_indices
+from typing import List
+import numpy as np
+from ..tiles import HipsSurveyProperties, HipsTile, HipsTileMeta
+from ..utils import WCSGeometry, compute_healpix_pixel_indices
 
 __all__ = [
-    'make_sky_image'
+    'draw_sky_image',
+    'make_sky_image',
 ]
 
 
-def make_sky_image(geometry: WCSGeometry, hips_survey: HipsSurveyProperties):
-    order = hips_survey.hips_order
-    healpix_pixel_indices = compute_healpix_pixel_indices(geometry, order)
-    tiles = list()
+def draw_sky_image(geometry: WCSGeometry, tiles: List[HipsTile]) -> np.ndarray:
+    """Draw sky image using the simple and quick method.
+
+    TODO: fill in code here
+    """
+    # TODO: copy over (and clean up a little) the drawing code you already have in notebooks.
+    # Suggestion: don't debug the code now, just put it here.
+    # First implement simple tile caching so that you can draw without needing to make web queries.
+
+    # Probably this function should just loop over tiles and call a `_draw_sky_image_one_tile`
+    # helper function, and sum up the results.
+
+    return np.zeros(geometry.shape)
+
+
+def _fetch_tiles(healpix_pixel_indices, order, hips_survey):
     for healpix_pixel_index in healpix_pixel_indices:
         tile_meta = HipsTileMeta(order=order, ipix=healpix_pixel_index, file_format='jpg')
-        tiles.append(HipsTile(tile_meta))
+        tile = HipsTile.fetch(tile_meta, hips_survey.base_url)
+        yield tile
+
+
+def make_sky_image(geometry: WCSGeometry, hips_survey: HipsSurveyProperties):
+    """Make sky image: fetch tiles and draw.
+
+    Parameters
+    ----------
+    geometry : `~hips.utils.WCSGeometry`
+        Geometry of the output image
+    hips_survey : `~hips.HipsSurveyProperties`
+        HiPS survey properties
+
+    Returns
+    -------
+    data : `~numpy.ndarray`
+        Output image pixels
+
+    Examples
+    --------
+    Define which image you want:
+
+    >>> geometry = tbd
+
+    Define which HiPS survey you want:
+
+    >>> hips_survey = tbd
+
+    Compute the image:
+
+    >>> from hips import make_sky_image
+    >>> data = make_sky_image(geometry, hips_survey)
+
+    If you want, you can save the image to file:
+    >>> from astropy.io import fits
+    >>> hdu = fits.PrimaryHDU(data=data, header=geometry.header)
+    >>> hdu.writeto('my_image.fits')
+    """
+    # Compute list of tiles needed
+    order = hips_survey.hips_order
+    healpix_pixel_indices = compute_healpix_pixel_indices(geometry, order)
+
+    # Fetch the tiles
+    # TODO: this isn't a good API. Will become better when we have a cache.
+    tiles = _fetch_tiles(healpix_pixel_indices, order, hips_survey)
+
+    # Draw the image
+    image_data = draw_sky_image(geometry, tiles)
+
+    return image_data
--- hips/draw/tests/test_simple.py
+++ hips/draw/tests/test_simple.py
@@ -1,21 +1,53 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
-
-from astropy.coordinates import SkyCoord
+import pytest
+import numpy as np
 from astropy.tests.helper import remote_data
+from ...tiles import HipsSurveyProperties, HipsTileMeta, HipsTile
+from ..simple import make_sky_image, draw_sky_image
+from ...utils.testing import get_hips_extra_file, make_test_wcs_geometry, requires_hips_extra
 
-from hips.tiles import HipsSurveyProperties
-from hips.utils import WCSGeometry
-from ..simple import make_sky_image
+
+def get_test_tiles():
+    # Could make manually a list of two tiles to draw
+    # Pointing to FITS tiles in hips-extra like e.g.
+    # hips-extra/datasets/samples//DSS2Red/Norder3/Dir0/Npix659.fits
+
+    # TODO: check if this tile is inside our image
+    tile1 = HipsTile.read(
+        meta=HipsTileMeta(order=3, ipix=659, file_format='fits', tile_width=512),
+        filename=get_hips_extra_file('datasets/samples//DSS2Red/Norder3/Dir0/Npix659.fits'),
+    )
+
+    # TODO: also fetch a second one.
+
+    return [tile1]
 
 
+@requires_hips_extra()
+def test_draw_sky_image():
+    # filename = get_pkg_data_filename('../../tiles/tests/data/properties.txt')
+    # hips_survey = HipsSurveyProperties.read(filename)
+    geometry = make_test_wcs_geometry(case=2)
+    tiles = get_test_tiles()
+
+    data = draw_sky_image(geometry, tiles)
+
+    assert data.shape == geometry.shape
+    assert data.dtype == np.float64
+
+    assert data[100, 200] == 0
+
+
+@pytest.mark.xfail
 @remote_data
 def test_make_sky_image():
     url = 'https://raw.githubusercontent.com/hipspy/hips/master/hips/tiles/tests/data/properties.txt'
     hips_survey = HipsSurveyProperties.fetch(url)
+    geometry = make_test_wcs_geometry(case=2)
 
-    shape = (1000, 2000)
-    y_center, x_center = shape[0] / 2, shape[1] / 2
-    skycoord = SkyCoord(0, 0, unit="deg")
-    wcs_geometry = WCSGeometry.create(skydir=skycoord, shape=shape, coordsys='CEL', projection='AIT', cdelt=0.01,
-                                      crpix=(y_center, x_center))
-    make_sky_image(wcs_geometry, hips_survey)
+    data = make_sky_image(geometry, hips_survey)
+
+    assert data.shape == geometry.shape
+    assert data.dtype == np.float64
+
+    assert data[100, 200] == 42
