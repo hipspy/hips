a0759cbe24b41ac50a86165db4438d474385291f
Adeel Ahmad <adeelahmad14@hotmail.com>
2017-06-30 11:11:23 +0500

    Remove test case for SimpleTilePainter class

--- hips/draw/tests/test_simple.py
+++ hips/draw/tests/test_simple.py
@@ -4,7 +4,7 @@ import pytest
 from astropy.utils.data import get_pkg_data_filename
 from numpy.testing import assert_allclose
 
-from ..simple import make_sky_image, draw_sky_image, SimpleTilePainter
+from ..simple import make_sky_image, draw_sky_image
 from ...tiles import HipsSurveyProperties, HipsTileMeta, HipsTile
 from ...utils.testing import get_hips_extra_file, make_test_wcs_geometry, requires_hips_extra
 
@@ -31,7 +31,6 @@ def get_test_tiles():
 def test_draw_sky_image():
     geometry = make_test_wcs_geometry(case=2)
     tiles = get_test_tiles()
-
     data = draw_sky_image(geometry, tiles)
 
     assert data.shape == geometry.shape
@@ -51,21 +50,3 @@ def test_make_sky_image():
     assert data.dtype == np.float64
     assert_allclose(data[200, 994], 3717.10091363)
     assert_allclose(data[200, 995], 3402.55292158)
-
-
-class TestSimpleTilePainter:
-    @classmethod
-    def setup_class(cls):
-        geometry = make_test_wcs_geometry(case=2)
-        tile = HipsTile.read(
-            meta=HipsTileMeta(order=3, ipix=450, file_format='fits', frame='icrs', tile_width=512),
-            filename=get_hips_extra_file('datasets/samples/DSS2Red/Norder3/Dir0/Npix450.fits'),
-        )
-        cls.simple_tile_painter = SimpleTilePainter(geometry, tile)
-
-    # def test_warp_image(self):
-    #     self.simple_tile_painter.warp_image()
-    #     assert_allclose(self.simple_tile_painter.tile.meta.skycoord_corners.ra.deg,
-    #                     [264.375, 258.75, 264.375, 270.])
-    #     assert_allclose(self.simple_tile_painter.tile.meta.skycoord_corners.dec.deg,
-    #                     [-24.624318, -30., -35.685335, -30.])
