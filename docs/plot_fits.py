eb5cd378624001de9588a4eb7095361b1f9b94d5
Adeel Ahmad <adeelahmad14@hotmail.com>
2017-07-11 20:29:14 +0500

    Reformat SimpleTilePainter class

--- docs/plot_fits.py
+++ docs/plot_fits.py
@@ -6,7 +6,6 @@ from astropy.visualization.mpl_normalize import simple_norm
 from hips import HipsSurveyProperties
 from hips import make_sky_image
 from hips.utils import WCSGeometry
-import numpy as np
 url = 'http://alasky.unistra.fr/DSS/DSS2Merged/properties'
 hips_survey = HipsSurveyProperties.fetch(url)
 geometry = WCSGeometry.create_simple(
@@ -14,7 +13,7 @@ geometry = WCSGeometry.create_simple(
     width=2000, height=1000, fov="3 deg",
     coordsys='galactic', projection='AIT'
 )
-data = make_sky_image(geometry=geometry, hips_survey=hips_survey, tile_format='fits')
+image = make_sky_image(geometry=geometry, hips_survey=hips_survey, tile_format='fits')
 ax = plt.subplot(projection=geometry.wcs)
-norm = simple_norm(data, 'sqrt')
-ax.imshow(data, origin='lower', norm=norm, cmap='gray')
+norm = simple_norm(image, 'sqrt')
+ax.imshow(image, origin='lower', norm=norm, cmap='gray')
--- hips/utils/tests/test_healpix.py
+++ hips/utils/tests/test_healpix.py
@@ -4,7 +4,7 @@ import numpy as np
 from numpy.testing import assert_allclose
 from astropy.coordinates import SkyCoord
 import healpy as hp
-from ..healpix import boundaries, compute_healpix_pixel_indices
+from ..healpix import boundaries, compute_healpix_pixel_indices, get_hips_order_for_resolution
 from ..testing import make_test_wcs_geometry
 
 
@@ -33,3 +33,17 @@ def test_wcs_healpix_pixel_indices(pars):
     geometry = make_test_wcs_geometry(case=2)
     healpix_pixel_indices = compute_healpix_pixel_indices(geometry, order=3, healpix_frame=pars['frame'])
     assert list(healpix_pixel_indices) == pars['ipix']
+
+hips_order_for_resolution_pars = [
+    dict(tile_width=512, resolution=0.01232, resolution_res=0.06395791924665553, order=4),
+    dict(tile_width=256, resolution=0.0016022, resolution_res=0.003997369952915971, order=8),
+    dict(tile_width=128, resolution=0.00009032, resolution_res=0.00012491781102862408, order=13),
+]
+
+
+@pytest.mark.parametrize('pars', hips_order_for_resolution_pars)
+def test_get_hips_order_for_resolution(pars):
+    hips_order = get_hips_order_for_resolution(pars['tile_width'], pars['resolution'])
+    assert hips_order == pars['order']
+    hips_resolution = hp.nside2resol(hp.order2nside(hips_order))
+    assert_allclose(hips_resolution, pars['resolution_res'])
