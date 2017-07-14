e365ccb15f523e8abbd688a14a673539e46ca7ed
Christoph Deil <Deil.Christoph@gmail.com>
2017-07-14 08:59:40 +0200

    Some code formatting in hips.utils

--- hips/utils/tests/test_healpix.py
+++ hips/utils/tests/test_healpix.py
@@ -22,26 +22,21 @@ def test_boundaries():
     assert_allclose(radec.dec.deg, [-24.624318, -30., -35.685335, -30.])
 
 
-compute_healpix_pixel_indices_pars = [
+@pytest.mark.parametrize('pars', [
     dict(frame='galactic', ipix=[269, 270, 271, 280, 282, 283, 292, 293, 295, 304, 305, 306]),
     dict(frame='icrs', ipix=[448, 449, 450, 451, 454, 456, 457, 460, 661, 663, 669]),
-]
-
-
-@pytest.mark.parametrize('pars', compute_healpix_pixel_indices_pars)
+])
 def test_wcs_healpix_pixel_indices(pars):
     geometry = make_test_wcs_geometry(case=2)
     healpix_pixel_indices = compute_healpix_pixel_indices(geometry, order=3, healpix_frame=pars['frame'])
     assert list(healpix_pixel_indices) == pars['ipix']
 
-hips_order_for_resolution_pars = [
+
+@pytest.mark.parametrize('pars', [
     dict(tile_width=512, resolution=0.01232, resolution_res=0.06395791924665553, order=4),
     dict(tile_width=256, resolution=0.0016022, resolution_res=0.003997369952915971, order=8),
     dict(tile_width=128, resolution=0.00009032, resolution_res=0.00012491781102862408, order=13),
-]
-
-
-@pytest.mark.parametrize('pars', hips_order_for_resolution_pars)
+])
 def test_get_hips_order_for_resolution(pars):
     hips_order = get_hips_order_for_resolution(pars['tile_width'], pars['resolution'])
     assert hips_order == pars['order']
