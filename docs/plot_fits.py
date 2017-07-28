78df9b79adc7c5022c237e7c1a74ddde2da23587
Christoph Deil <Deil.Christoph@gmail.com>
2017-07-28 11:00:08 +0200

    Change docs examples to use survey ID and Glimps for JPG example

--- docs/plot_fits.py
+++ docs/plot_fits.py
@@ -1,15 +1,14 @@
 """Basic example how to plot a sky image with the hips package"""
 from astropy.coordinates import SkyCoord
-from hips import WCSGeometry, HipsSurveyProperties, make_sky_image
+from hips import WCSGeometry, make_sky_image
 
 # Compute the sky image
-url = 'http://alasky.unistra.fr/DSS/DSS2Merged/properties'
-hips_survey = HipsSurveyProperties.fetch(url)
 geometry = WCSGeometry.create(
     skydir=SkyCoord(0, 0, unit='deg', frame='galactic'),
     width=2000, height=1000, fov="3 deg",
     coordsys='galactic', projection='AIT',
 )
+hips_survey = 'CDS/P/DSS2/red'
 result = make_sky_image(geometry=geometry, hips_survey=hips_survey, tile_format='fits')
 result.plot()
 
