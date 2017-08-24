96bd97f0990afcb6bd4475384cf4668f941385a0
Christoph Deil <Deil.Christoph@gmail.com>
2017-08-24 23:12:34 +0200

    Improve tile fetch code a bit

--- hips/tiles/fetch.py
+++ hips/tiles/fetch.py
@@ -14,15 +14,16 @@ __doctest_skip__ = [
 ]
 
 
-def fetch_tiles(tile_metas: List[HipsTileMeta], hips_survey : HipsSurveyProperties,
+def fetch_tiles(tile_metas: List[HipsTileMeta], hips_survey: HipsSurveyProperties,
                 progress_bar: bool = False, n_parallel: int = 10,
-                timeout: float = 10, fetch_package : str = 'urllib') -> List[HipsTile]:
+                timeout: float = 10, fetch_package: str = 'urllib') -> List[HipsTile]:
     """Fetch a list of HiPS tiles.
 
     This function fetches a list of HiPS tiles based
-    on their URLs, which are generated using `hips_survey`
-    and `tile_metas`. The tiles are then fetched asynchronously
-    using urllib or aiohttp.
+    on their URLs, which are generated using ``hips_survey``
+    and ``tile_metas``.
+
+    The tiles are then fetched asynchronously using ``urllib`` or ``aiohttp``.
 
     Parameters
     ----------
@@ -41,25 +42,26 @@ def fetch_tiles(tile_metas: List[HipsTileMeta], hips_survey : HipsSurveyProperti
 
     Examples
     --------
-    >>> from hips import HipsSurveyProperties
-    >>> from hips import fetch_tiles
-    >>> url = 'http://alasky.unistra.fr/DSS/DSS2Merged/properties'
-    >>> hips_survey = HipsSurveyProperties.fetch(url)
-    >>> tile_indices = [69623, 69627, 69628, 69629, 69630, 69631]
-    >>> tile_metas = []
-    >>> for healpix_pixel_index in tile_indices:
-    ...    tile_meta = HipsTileMeta(
-    ...        order=7,
-    ...        ipix=healpix_pixel_index,
-    ...        frame=hips_survey.astropy_frame,
-    ...        file_format='fits',
-    ...    )
-    ...    tile_metas.append(tile_meta)
-    >>> tiles = fetch_tiles(tile_metas, hips_survey)
-    >>> tiles[0].meta.file_format
-    fits
-    >>> tiles[0].meta.order
-    7
+    Define a list of tiles we want::
+
+        from hips import HipsSurveyProperties, HipsTileMeta
+        from hips import fetch_tiles
+        url = 'http://alasky.unistra.fr/DSS/DSS2Merged/properties'
+        hips_survey = HipsSurveyProperties.fetch(url)
+        tile_indices = [69623, 69627, 69628, 69629, 69630, 69631]
+        tile_metas = []
+        for healpix_pixel_index in tile_indices:
+            tile_meta = HipsTileMeta(
+               order=7,
+               ipix=healpix_pixel_index,
+               frame=hips_survey.astropy_frame,
+               file_format='fits',
+           )
+           tile_metas.append(tile_meta)
+
+    Fetch all tiles (in parallel)::
+
+        tiles = fetch_tiles(tile_metas, hips_survey)
 
     Returns
     -------
@@ -67,11 +69,25 @@ def fetch_tiles(tile_metas: List[HipsTileMeta], hips_survey : HipsSurveyProperti
         A Python list of HiPS tiles
     """
     if fetch_package == 'aiohttp':
-        return tiles_aiohttp(tile_metas, hips_survey, progress_bar)
+        fetch_fct = tiles_aiohttp
     elif fetch_package == 'urllib':
-        return tiles_urllib(tile_metas, hips_survey, progress_bar, n_parallel, timeout)
+        fetch_fct = tiles_urllib
     else:
         raise ValueError(f'Invalid package name: {fetch_package}')
+
+    tiles = fetch_fct(tile_metas, hips_survey, progress_bar, n_parallel, timeout)
+
+    # Sort tiles to match the tile_meta list
+    # TODO: this doesn't seem like a great solution.
+    # Use OrderedDict instead?
+    out = []
+    for tile_meta in tile_metas:
+        for tile in tiles:
+            if tile.meta == tile_meta:
+                out.append(tile)
+                continue
+    return out
+
 
 def fetch_tile_urllib(url: str, meta: HipsTileMeta, timeout: float) -> Generator:
     """Fetch a HiPS tile asynchronously."""
@@ -79,35 +95,38 @@ def fetch_tile_urllib(url: str, meta: HipsTileMeta, timeout: float) -> Generator
         raw_data = conn.read()
         return HipsTile(meta, raw_data)
 
-def tiles_urllib(tile_metas: List[HipsTileMeta], hips_survey : HipsSurveyProperties,
-                 progress_bar: bool = False, n_parallel: int = 10,  timeout: float = 10) -> List[HipsTile]:
+
+def tiles_urllib(tile_metas: List[HipsTileMeta], hips_survey: HipsSurveyProperties,
+                 progress_bar: bool = False, n_parallel: int = 10, timeout: float = 10) -> List[HipsTile]:
     """Generator function to fetch HiPS tiles from a remote URL."""
     with concurrent.futures.ThreadPoolExecutor(max_workers=n_parallel) as executor:
         futures = []
         for meta in tile_metas:
             url = hips_survey.tile_url(meta)
-            futures.append(executor.submit(fetch_tile_urllib, url, meta, timeout))
+            future = executor.submit(fetch_tile_urllib, url, meta, timeout)
+            futures.append(future)
 
+        futures = concurrent.futures.as_completed(futures)
         if progress_bar:
-            from tqdm import tqdm as progress_bar
-        else:
-            def progress_bar(*args):
-                return args[0]
+            from tqdm import tqdm
+            futures = tqdm(futures, total=len(tile_metas), desc='Fetching tiles')
 
         tiles = []
-        for future in progress_bar(concurrent.futures.as_completed(futures), total=len(futures), desc='Fetching tiles'):
+        for future in futures:
             tiles.append(future.result())
 
     return tiles
 
-async def fetch_tile_aiohttp(url: str, meta : HipsTileMeta, session) -> Generator:
+
+async def fetch_tile_aiohttp(url: str, meta: HipsTileMeta, session) -> Generator:
     """Fetch a HiPS tile asynchronously using aiohttp."""
     async with session.get(url) as response:
         raw_data = await response.read()
         return HipsTile(meta, raw_data)
 
+
 async def fetch_all_tiles_aiohttp(tile_metas: List[HipsTileMeta],
-                                  hips_survey : HipsSurveyProperties, progress_bar: bool) -> List[HipsTile]:
+                                  hips_survey: HipsSurveyProperties, progress_bar: bool) -> List[HipsTile]:
     """Generator function to fetch HiPS tiles from a remote URL using aiohttp."""
     import aiohttp
 
@@ -115,22 +134,24 @@ async def fetch_all_tiles_aiohttp(tile_metas: List[HipsTileMeta],
         futures = []
         for meta in tile_metas:
             url = hips_survey.tile_url(meta)
-            futures.append(asyncio.ensure_future(fetch_tile_aiohttp(url, meta, session)))
+            future = asyncio.ensure_future(fetch_tile_aiohttp(url, meta, session))
+            futures.append(future)
 
+        futures = asyncio.as_completed(futures)
         if progress_bar:
-            from tqdm import tqdm as progress_bar
-        else:
-            def progress_bar(*args):
-                return args[0]
+            from tqdm import tqdm
+            futures = tqdm(futures, total=len(tile_metas), desc='Fetching tiles')
 
         tiles = []
-        for future in progress_bar(asyncio.as_completed(futures), total=len(futures), desc='Fetching tiles'):
+        for future in futures:
             tiles.append(await future)
 
     return tiles
 
-def tiles_aiohttp(tile_metas: List[HipsTileMeta], hips_survey : HipsSurveyProperties,
-                  progress_bar: bool) -> List[HipsTile]:
+
+def tiles_aiohttp(tile_metas: List[HipsTileMeta], hips_survey: HipsSurveyProperties,
+                  progress_bar: bool, n_parallel: int = 10, timeout: float = 10) -> List[HipsTile]:
+    # TODO: implement n_parallel and timeout
     return asyncio.get_event_loop().run_until_complete(
         fetch_all_tiles_aiohttp(tile_metas, hips_survey, progress_bar)
     )
--- hips/tiles/tests/test_fetch.py
+++ hips/tiles/tests/test_fetch.py
@@ -8,24 +8,34 @@ from ..tile import HipsTileMeta
 
 TILE_FETCH_TEST_CASES = [
     dict(
-        tile_indices=[69623],
+        tile_indices=[69623, 69627, 69628, 69629, 69630, 69631],
         tile_format='fits',
         order=7,
         url='http://alasky.unistra.fr/DSS/DSS2Merged/properties',
         progress_bar=True,
-        data=[2101, 1680, 1532, 1625, 2131],
-        fetch_package='urllib'
+        data=[2101, 1945, 1828, 1871, 2079, 2336],
+        fetch_package='urllib',
     ),
     dict(
-        tile_indices=[69623],
+        tile_indices=[69623, 69627, 69628, 69629, 69630, 69631],
         tile_format='fits',
         order=7,
         url='http://alasky.unistra.fr/DSS/DSS2Merged/properties',
         progress_bar=True,
-        data=[2101, 1680, 1532, 1625, 2131],
-        fetch_package='aiohttp'
+        data=[2101, 1945, 1828, 1871, 2079, 2336],
+        fetch_package='aiohttp',
     ),
 ]
+
+
+def make_tile_metas(hips_survey, pars):
+    for healpix_pixel_index in pars['tile_indices']:
+        yield HipsTileMeta(
+            order=pars['order'],
+            ipix=healpix_pixel_index,
+            frame=hips_survey.astropy_frame,
+            file_format=pars['tile_format'],
+        )
 
 
 @pytest.mark.parametrize('pars', TILE_FETCH_TEST_CASES)
@@ -33,15 +43,13 @@ TILE_FETCH_TEST_CASES = [
 def test_fetch_tiles(pars):
     hips_survey = HipsSurveyProperties.fetch(pars['url'])
 
-    tile_metas = []
-    for healpix_pixel_index in pars['tile_indices']:
-        tile_meta = HipsTileMeta(
-            order=pars['order'],
-            ipix=healpix_pixel_index,
-            frame=hips_survey.astropy_frame,
-            file_format=pars['tile_format'],
-        )
-        tile_metas.append(tile_meta)
+    tile_metas = list(make_tile_metas(hips_survey, pars))
 
-    tiles = fetch_tiles(tile_metas, hips_survey, progress_bar=pars['progress_bar'], fetch_package=pars['fetch_package'])
-    assert_allclose(tiles[0].data[0][5:10], [2101, 1680, 1532, 1625, 2131])
+    tiles = fetch_tiles(
+        tile_metas, hips_survey,
+        progress_bar=pars['progress_bar'],
+        fetch_package=pars['fetch_package'],
+    )
+
+    for idx, val in enumerate(pars['data']):
+        assert_allclose(tiles[idx].data[0][5], val)
