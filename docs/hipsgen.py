866748ac20b0ab91c7829a34b37e3b288124eba5
Christoph Deil <Deil.Christoph@gmail.com>
2019-01-10 17:23:52 +0100

    Improve hipsgen

--- docs/hipsgen.py
+++ docs/hipsgen.py
@@ -9,19 +9,35 @@ from astropy.coordinates import SkyCoord
 from astropy_healpix import HEALPix
 
 
-def make_healpix_data():
+def make_healpix_data(file_format):
     """Silly example of HEALPix data.
-    Angular distance to
+
+    Angular distance in deg to (0, 0) as pixel values.
     """
     healpix = HEALPix(nside=4)
     ipix = np.arange(healpix.npix)
     lon, lat = healpix.healpix_to_lonlat(ipix)
     coord = SkyCoord(lon, lat)
-    center = SkyCoord(0, 0, unit='deg')
-    return coord.separation(center).deg
+    center = SkyCoord(0, 0, unit="deg")
+    data = coord.separation(center).deg
+
+    if file_format == "fits":
+        return data
+    elif file_format == "jpg":
+        data = data.astype('uint8')
+        return np.moveaxis([data, data + 1, data + 2], 0, -1)
+    elif file_format == "png":
+        data = data.astype('uint8')
+        return np.moveaxis([data, data + 1, data + 2, data + 3], 0, -1)
+    else:
+        raise ValueError()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
-    data = make_healpix_data()
-    hips.healpix_to_hips(data, tile_width=4, base_path='test123', file_format='fits')
+
+    file_format = "png"
+    data = make_healpix_data(file_format)
+    hips.healpix_to_hips(
+        data, tile_width=4, base_path="test123", file_format=file_format
+    )
--- hips/draw/hipsgen.py
+++ hips/draw/hipsgen.py
@@ -1,11 +1,11 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 import logging
 import numpy as np
-from ..tiles import HipsSurveyProperties
+from ..tiles import HipsSurveyProperties, HipsTileAllskyArray, HipsTile, HipsTileMeta
 from .healpix import healpix_to_hips_tiles
 from pathlib import Path
 
-__all__ = ["healpix_to_hips"]
+__all__ = ["healpix_to_hips", "HipsSurvey"]
 
 log = logging.getLogger(__name__)
 
@@ -28,7 +28,7 @@ HTML_TEMPLATE = r"""
 
     aladin.setImageSurvey(
         aladin.createImageSurvey(
-            '{name}', '{name}', '', 'galactic', 1, {{imgFormat: '{imgFormat}'}}
+            '{name}', '{name}', '.', 'galactic', 1, {{imgFormat: '{imgFormat}'}}
         )
     );
 
@@ -66,24 +66,20 @@ def healpix_to_hips(
     base_path.mkdir(exist_ok=True, parents=True)
     path = base_path / "properties"
     log.info(f"Writing {path}")
-    HipsSurveyProperties(
+    properties = HipsSurveyProperties(
         {
             "hips_tile_format": file_format,
             "hips_tile_width": tile_width,
             "hips_frame": frame,
         }
-    ).write(path)
+    )
+    properties.write(path)
 
     # Make and write index.html
-    txt = HTML_TEMPLATE.format_map({
-        "name": "test123",
-        "imgFormat": "fits",
-    })
+    txt = HTML_TEMPLATE.format_map({"name": "test123", "imgFormat": "fits"})
     path = base_path / "index.html"
     log.info(f"Writing {path}")
     path.write_text(txt)
-
-    # TODO: make and write Allsky.{file_format}
 
     # Make and write tiles
     for tile in healpix_to_hips_tiles(hpx_data, tile_width, file_format, frame):
@@ -91,3 +87,62 @@ def healpix_to_hips(
         log.info(f"Writing {path}")
         path.parent.mkdir(exist_ok=True, parents=True)
         tile.write(path)
+
+    # Make and write allsky file
+    hips_survey = HipsSurvey(base_path)
+    allsky = hips_survey.make_allsky(order=0, file_format=file_format)
+    path = base_path / f"Norder0/Allsky.{file_format}"
+    log.info(f"Writing {path}")
+    allsky.write(path)
+
+
+# TODO: generalise and move to a better location
+class HipsSurvey:
+    """HiPS survey container.
+
+    Represents one HiPS survey, acting as a manager
+    to interact with the various pieces that make up a HiPS:
+
+    - One `HipsSurveyProperties`
+    - Many `HipsTile`
+    - One `HipsTileAllskyArray` per order
+
+    TODO: do we need several classes, for the different cases,
+    or can they be handled by one `HipsSurvey` class?
+
+    - in memory
+    - on local disk
+    - on server
+
+    Functionality should include:
+
+    - locate, read, write files
+    - scan and print summary of contents (e.g. which orders / tiles are present)
+    - copy / clone HiPS (possibly only small parts, with selections) locally and from servers
+    - generate allsky from tiles? put this functionality here or somewhere else?
+
+    Parameters
+    ----------
+    base_path : `pathlib.Path`
+        Base path (folder where the ``properties`` file is located)
+    """
+
+    def __init__(self, base_path):
+        self.base_path = base_path
+
+    def make_allsky(self, order: int, file_format: str) -> HipsTileAllskyArray:
+        tiles = self.read_tiles(order, file_format)
+        # TODO: make from_tiles work with generators. For now we have to make a list
+        tiles = list(tiles)
+        return HipsTileAllskyArray.from_tiles(tiles)
+
+    def read_tiles(self, order: int, file_format: str):
+        properties = HipsSurveyProperties.read(self.base_path / 'properties')
+        tile_path = self.base_path / f'Norder{order}'
+
+        for path in tile_path.glob(f"Dir*/Npix*.{file_format}"):
+            ipix = int(path.as_posix().split('/')[-1].split('.')[0][4:])
+            frame = properties.hips_frame
+            width = properties.tile_width
+            meta = HipsTileMeta(order, ipix, file_format, frame, width)
+            yield HipsTile.read(meta, path)
